f79fd3bae6f7f756c9254db6906a05efc1b72a5c
kangbolu <kangbolu@student.ubc.ca>
2020-11-19 17:06:31 +0800

    add new optional argument

---
+++ demo.R
@@ -0,0 +1,19 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"This script prints out docopt args.
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
+" -> doc
+
+library(docopt)
+
+opt <- docopt(doc)
+print(opt$arg4)
+print(opt)
+print(typeof(opt))
